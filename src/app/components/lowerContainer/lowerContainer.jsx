0e8af80a4b9bff36bc1c7fbb691ecc2ff356dbbc
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-04-25 17:09:54 -0700

    Add props to room menu component
    Co-authored-by: eswitbeck <eswitbeck@users.noreply.github.com>

--- src/app/components/lowerContainer/lowerContainer.jsx
+++ src/app/components/lowerContainer/lowerContainer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './lowerContainerStyle.scss';
 import FormContainer from '../formContainer/formContainer.jsx';
 import Room from '../room/roomContainer.jsx';
+
+//render room component
+//render form container componenet
+
 export default function LowerContainer() {
   return (
     <div className="lowerContainer">
