762e0a03fbda437c1a032ad7a1457c1061f6bb6b
Lilianne Mueller <lilyanneom@gmail.com>
2023-04-25 20:14:59 -0700

    build oout routes, controllers for day, room, waterings, plants, users

--- src/server/routers/dayRouter.js
+++ src/server/routers/dayRouter.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 const dayController = require('../controllers/dayController');
 
-// router.get();
-// router.post();
-// router.delete();
-// router.put();
+router.get('/user/:id', dayController.getAllDays, (req, res) => {
+  res.status(200).json(res.locals.days);
+});
 
+router.get('/:id', dayController.getDay, (req, res) => {
+  res.status(200).json(res.locals.day);
+});
 
-module.exports = router
+router.post('/', dayController.addDay, (req, res) => {
+  res.status(201).json(res.locals.day);
+});
+
+router.delete('/:id', dayController.deleteDay, (req, res) => {
+  res.status(200).json(res.locals.day);
+});
+
+// router.put('/:id', dayController.updateDay, (req, res) => {
+//   res.status(200).json(res.locals.day);
+// });
+
+module.exports = router;
