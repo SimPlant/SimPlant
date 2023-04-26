e8db2b9a651ffddf2897f5a684d721ca2090e915
alexyam0 <alexyam001@gmail.com>
2023-04-26 10:38:30 -0700

    switching branches

--- src/server/apiController.js
+++ src/server/apiController.js
@@ -4,9 +4,14 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
+const { Pool } = require('pg');
 
 //this method adds dotenv variables to our process
 dotenv.config();
+const PG_URI = process.env.PG_URI;
+const pool = new Pool({
+  connectionString: PG_URI
+});
 const TOKEN = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
@@ -41,23 +46,6 @@ const apiController = {
     
         const plantId = res.locals.plantId;
 
-        // await plantId.forEach( async (plantName) => {
-        //    try {
-        //       const data = await fetch(`https://open.plantbook.io/api/v1/plant/detail/${plantName}/`, {
-        //         method: 'GET',
-        //         headers: { 'Authorization': `Bearer ${TOKEN}` }
-        //       })
-        //       const result = data.json()
-        //       plantInfo.push(result)
-        //    }
-        //    catch {
-        //       return next({
-        //         log: 'getPlantDetails',
-        //         message: { err: 'Error getting plan details' }
-        //       })
-        //    } 
-        // })
-
         for (let plantName of plantId) {
           await fetch(`https://open.plantbook.io/api/v1/plant/detail/${plantName}/`, {
                 method: 'GET',
@@ -71,45 +59,7 @@ const apiController = {
 
         res.locals.plantDetails = plantInfo
         return next()
-
-        // console.log('received array from getPlantFamily: ', plantId);
-        // const promises = plantId.map( async (plantName) => {
-        // plantId.map( async (plantName) => {
-        // try { 
-        //     const data = await fetch(`https://open.plantbook.io/api/v1/plant/detail/${plantName}/`, {
-        //         method: 'GET',
-        //         headers: { 'Authorization': `Bearer ${TOKEN}` }
-        //       })
-        //     const newData = await data.json()
-        //     plantInfo.push(newData);
-        //     console.log("inside: ", plantInfo)
-        //     }
-        // catch {
-        //     const message = 'error encountered in apiController.getPlantDetails: ' + err;
-        //     return next(message);
-        //   }
-        // try {
-        //     console.log('plantId from second try block: ', plantInfo);
-        //     res.locals.plantDetails = plantInfo;
-        //     return next();
-        // }
-        // catch {
-        //   const message = 'error encountered in apiController.getPlantDetails: ' + err;
-        //   return next(message);
-        // }
-      // });
-
-        // console.log("promises", await promises);
-        // try{
-        //   res.locals.plantDetails = await Promise.all(promises);
-        //   console.log('outside:', await res.locals.plantDetails);
-        //   // console.log('plantDetails', await res.locals.plantDetails)
-        //   await next();
-        // }
-        // catch(err){
-        //   const message = 'error encountered in apiController.getPlantDetails: ' + err;
-        //   return next(message);
-        //}
+        
       },
     
       //add plant and its details to db
@@ -136,7 +86,7 @@ const apiController = {
                           RETURNING *;`
             const result = await pool.query(query, [pid, max_light_lux, min_light_lux, max_temp, min_temp, max_env_humid, min_env_humid, max_soil_moist, min_soil_moist, watering_frequency_per_week, image_url]);              
           } catch (error) {
-            const message = 'error encountered in apiController.addPlantToDB: ' + err;
+            const message = 'error encountered in apiController.addPlantToDB: ' + `${error}`;
             return next(message);
           }
         });
--- src/server/routes/apiRoutes.js
+++ src/server/routes/apiRoutes.js
@@ -11,7 +11,7 @@ const apiController = require('../apiController.js')
 //  apiController.addPlantToDB, getPlantDetails,
 // on client's call to /plantFamily > server calls plant API > store response to db
 
-router.get('/plantFamily/:family', apiController.getPlantFamily, apiController.getPlantDetails, (req, res) => {
+router.get('/plantFamily/:family', apiController.getPlantFamily, apiController.getPlantDetails, apiController.addPlantToDB, (req, res) => {
     return res.status(200).send(res.locals.plantDetails);
 });
 
