bd5819b886023451378e6776593fb344db21dcb3
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-27 11:41:21 -0700

    Integrate modal and drag-n-drop (and resolve bugs)
    Co-authored-by: boilerpot <boilerpot@users.noreply.github.com>
    Co-authored-by: Lane <LaneEcho@users.noreply.github.com>
    Co-authored-by: ectjon <ectjon@users.noreply.github.com>
    Co-authored-by: Lilianne Mueller <liliannemueller@users.noreply.github.com>

--- src/app/components/plants/plant.jsx
+++ src/app/components/plants/plant.jsx
@@ -3,7 +3,6 @@ import './plantStyle.scss'
 
 const Plant = (props) => {
   // state for modal functionality
-  const [isDropdown, setIsDropdown] = useState(false);
  
 let common_name;
 let watering_frequency_per_week;
@@ -18,30 +17,20 @@ if (props?.plant){
     light 
   } = props.plant);
 }
-// state tells div what to render
-
-  function openDropdown(e) {
-    e.target.focus();
-    setIsDropdown(true);
-  }
-
-  function closeDropdown() {
-    setIsDropdown(false);
-  }
 
 
   return(
-    <div className={`plant ${isDropdown ? 'plant-dropdown' : ''}`} onClick={openDropdown} tabIndex="-1" onBlur={closeDropdown}>
-      <p className="species">Name: {common_name}</p>
-      <img className= "plant-img" src="https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png"></img>
-      { isDropdown && 
-        <>
-          <p>Water {watering_frequency_per_week}x per week</p>
-          <p>Light: {light}/10</p>
-          <button>Delete</button>
-        </>
-      }
-    </div>
+      <>
+        <p className="species">Name: {common_name}</p>
+        <img className= "plant-img" src="https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png"></img>
+        { props.isDropdown && 
+          <>
+            <p>Water {watering_frequency_per_week}x per week</p>
+            <p>Light: {light}/10</p>
+            <button>Delete</button>
+          </>
+        }
+      </>
   );
 }
 
