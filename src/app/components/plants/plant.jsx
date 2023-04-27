2fd569d64a6428853dea0534793bb195f957b5bf
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-27 07:52:40 -0700

    styling fiddling to try things out

--- src/app/components/plants/plant.jsx
+++ src/app/components/plants/plant.jsx
@@ -6,15 +6,20 @@ const Plant = (props) => {
   const [isDropdown, setIsDropdown] = useState(false);
  
 let common_name;
-let watering_frequency_per_week;
+let days_between_watering;
 let light;
 let species;
+let image;
+
+props?.plant?.image
+  ? image = props.plant.image
+  : image = "https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png";
 
 
 if (props?.plant){
   ({ species, 
     common_name,
-    watering_frequency_per_week, 
+    days_between_watering, 
     light 
   } = props.plant);
 }
@@ -29,16 +34,16 @@ if (props?.plant){
     setIsDropdown(false);
   }
 
-
   return(
     <div className={`plant ${isDropdown ? 'plant-dropdown' : ''}`} onClick={openDropdown} tabIndex="-1" onBlur={closeDropdown}>
-      <p className="species">Name: {common_name}</p>
-      <img className= "plant-img" src="https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png"></img>
+      <p className="title">Name: {common_name}</p>
+      <img className= "plant-img" src={image}></img>
       { isDropdown && 
         <>
-          <p>Water {watering_frequency_per_week}x per week</p>
+          <p>Species: {species}</p>
+          <p>Water every {days_between_watering} days</p>
           <p>Light: {light}/10</p>
-          <button>Delete</button>
+          <button className="delete">Delete</button>
         </>
       }
     </div>
