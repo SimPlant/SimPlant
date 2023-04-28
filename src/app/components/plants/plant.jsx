23d142217bc20fca50db43b887f17b444f15f507
Esther Witbeck <esther.witbeck@gmail.com>
2023-04-27 17:15:58 -0700

    Tidy delete button; handle styling (reshaping room); hande flex display.
    Co-authored-by: boilerpot <boilerpot@users.noreply.github.com>
    Co-authored-by: Lane <LaneEcho@users.noreply.github.com>
    Co-authored-by: Lilianne Mueller <liliannemueller@users.noreply.github.com>
    Co-authored-by: ectjon <ectjon@users.noreply.github.com>

--- src/app/components/plants/plant.jsx
+++ src/app/components/plants/plant.jsx
@@ -8,11 +8,6 @@ let common_name;
 let days_between_watering;
 let light;
 let species;
-let image;
-
-props?.plant?.image
-  ? image = props.plant.image
-  : image = "https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png";
 
 
 if (props?.plant){
@@ -23,23 +18,19 @@ if (props?.plant){
   } = props.plant);
 }
 
-  function handleDeletePlant (e){
-    const plantID = Number(e.target.parentNode.getAttribute('value'));
-    props.deletePlant(plantID);
-  }
 
   return(
       <>
         <p className="title">Name: {common_name}</p>
-        <img className= "plant-img" src={image}></img>
+        <img className= "plant-img" src={props?.plant.image || "https://em-content.zobj.net/thumbs/160/apple/271/potted-plant_1fab4.png"}></img>
         { props.isDropdown && 
           <>
             <p>Species: {species}</p>
-            <p>Water every {days_between_watering} days</p>
+            <p>Water {days_between_watering}x per week</p>
             <p>Light: {light}/10</p>
+            <button onMouseDown={e => props.deletePlant(props.plant._id)} className="delete">Delete</button>
           </>
         }
-        <button onClick={handleDeletePlant}>Delete</button>
       </>
   );
 }
